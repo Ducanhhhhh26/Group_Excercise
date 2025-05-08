5d5e0c4205974cd19dc46715db3861f69047cad3
unknown <ducanhcualo2403@gmail.com>
2025-05-08 13:41:28 +0700

    update

--- js/Admin-artists.js
+++ js/Admin-artists.js
@@ -254,7 +254,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     return new Promise((resolve) => {
       const reader = new FileReader();
-      reader.onload = () => resolve(reader.result);
+      reader.onload = () => resolve(reader.result); 
       reader.onerror = () => resolve(null);
       reader.readAsDataURL(file);
     });
